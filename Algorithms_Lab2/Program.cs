2d92e3f88f2347cb1f16171b9ccd2f0c119126a3
Гребенкин Алексей <24gas2001@gmail.com>
2021-03-30 19:40:20 +0700

    Добавьте файлы проекта.

---
+++ Algorithms_Lab2/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace Algorithms_Lab2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RBTree tree = null;
+            while (true)
+            {
+                Console.WriteLine("Меню:");
+                Console.WriteLine("0 Добавленеие элемента");
+                Console.WriteLine("1 Удаление элемента");
+                Console.WriteLine("2 Вывод дерева");
+
+                int a = Int32.Parse(Console.ReadLine());
+                int value=0;
+
+                if (a<2)
+                {
+                    Console.WriteLine("Введите значение элемента");
+                    value = Int32.Parse(Console.ReadLine());
+                }
+
+                switch (a)
+                {
+                    case 0:
+                        {
+                            if (tree == null)
+                                tree = new RBTree(value);
+                            else
+                                tree.Add(value);
+                            break;
+                        }
+                    case 1:
+                        {
+                            if (tree == null)
+                                Console.WriteLine("Дерево пусто");
+                            else
+                                tree.Remove(value);
+                            break;
+                        }
+                    case 2:
+                        {
+                            if (tree == null)
+                                Console.WriteLine("Дерево пусто");
+                            else
+                                tree.Print();
+                            break;
+                        }
+                }
+            }
+        }
+    }
+}
---
+++ Algorithms_Lab2/RBTree.cs
@@ -0,0 +1,196 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithms_Lab2
+{
+    public enum ColorNode{
+        RED,
+        BLACK   
+    }
+    class RBTree
+    {
+        private RBTree Parent
+        {
+            get; set;
+        }
+        private RBTree Left
+        {
+            get; set;
+        }
+        private RBTree Right
+        {
+            get; set;
+        }
+        public int Value
+        {
+            get; set;
+        }
+        private ColorNode _color;
+        private Queue<RBTree> _queuePrint = new Queue<RBTree>();
+        public RBTree(int value, RBTree parent = null)
+        {
+            Value = value;
+            Parent = parent;
+        }
+
+        public void Add(int value)
+        {
+            if (value < Value)
+            {
+                if (Left == null)
+                {
+                    Left = new RBTree(value, this);
+                }
+                else
+                {
+                    Left.Add(value);
+                }
+            }
+            else
+            {
+                if (Right == null)
+                {
+                    Right = new RBTree(value, this);
+                }
+                else
+                {
+                    Right.Add(value);
+                }
+            }
+        }
+
+        private RBTree Search (int value)
+        {
+            if (this == null)
+                return null;
+            if (value < Value)
+                return Left?.Search(value)??null;
+            if (value > Value)
+                return Right?.Search(value)??null;
+            if (Value == value)
+                return this;
+            return null;
+        }
+
+
+        private void Remove (RBTree tree)
+        {
+            if (tree == null)
+                return;
+
+            //Корень
+            if(tree.Parent == null)
+            {
+                if (tree.Left != null)
+                {
+                    tree.Value = tree.Left.Value;
+                    Remove(tree.Left);
+                }
+                else if (tree.Right != null)
+                {
+                    tree.Value = tree.Right.Value;
+                    Remove(tree.Right);
+                }
+                else
+                    ;//TODO: удаление последнего элемента
+                return;
+            }
+            //Лист
+            if (tree.Left == null && tree.Right == null)
+            {
+                if (tree.Parent.Left == tree)
+                    tree.Parent.Left = null;
+                else
+                    tree.Parent.Right = null;
+            }
+
+            //Узел с левым поддеревом
+            if (tree.Left != null && tree.Right == null)
+            {
+                tree.Left.Parent = tree.Parent;
+                if (tree.Parent.Left == tree)
+                    tree.Parent.Left = tree.Left;
+                else
+                    tree.Parent.Right = tree.Left;
+            }
+
+            ///Узел с правым поддеревом
+            if (tree.Left == null && tree.Right != null)
+            {
+                tree.Right.Parent = tree.Parent;
+                if (tree.Parent.Left == tree)
+                    tree.Parent.Left = tree.Right;
+                else
+                    tree.Parent.Right = tree.Right;
+            }
+
+            ///Узел с правым и левым поддеревом
+            if (tree.Left != null && tree.Right != null)
+            {
+                RBTree current = tree.Right;
+                while (current.Left != null)
+                    current = current.Left;
+
+                tree.Value = current.Value;
+                Remove(current);
+            }
+
+
+
+        }
+
+        public void Remove(int value)
+        {
+            RBTree tree = Search(value);
+            Remove(tree);
+        }
+
+
+        private void Print (RBTree tree)
+        {
+            if (tree.Left!=null)
+                Print(tree.Left);
+            Console.Write(tree.Value + " ");
+            if (tree.Right != null)
+                Print(tree.Right);
+        }
+
+        //public void Print()
+        //{
+        //    int pow = 0;
+        //    int count = 0;
+        //    _queuePrint.Enqueue(this);
+        //    Console.WriteLine();
+        //    while (_queuePrint.Count!=0)
+        //    {
+        //        count++;
+        //        RBTree tree = _queuePrint.Dequeue();
+        //        if (tree != null)
+        //        {
+        //            //if (tree.Left != null)
+        //            _queuePrint.Enqueue(tree.Left);
+        //            //if (tree.Right != null)
+        //            _queuePrint.Enqueue(tree.Right);
+        //        }
+        //        string value = tree?.Value.ToString()??"[]";
+        //        Console.Write(value + " ");
+        //        if (count == Math.Pow(2, pow))
+        //        {
+        //            Console.WriteLine();
+        //            pow++;
+        //        }
+        //    }
+        //    Console.WriteLine();
+        //    Console.WriteLine();
+        //}
+
+        public void Print()
+        {
+            Console.WriteLine();
+            Print(this);
+            Console.WriteLine();
+            Console.WriteLine();
+        }
+    }
+}
